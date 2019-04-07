77f4d9d59a15af72fa57c7edf8a70ba7ceb48584
Gram <gram7gram@gmail.com>
2019-04-08 00:23:51 +0300

    #master in progress

--- app/db.js
+++ app/db.js
@@ -5,19 +5,43 @@ const url = "mongodb://db:27017/dialogflow";
 MongoClient.connect(url, (err, db) => {
     if (err) throw err;
     console.log("Connected!");
-    db.close();
 });
 
-const createReminder = (date, time, content) => {
+const createReminder = async (date, time, content) => {
 
+	const entity = {
+		date, time, content
+	}
+
+	try {
+		const record = await dbo.collection("reminders").insertOne(entity)
+
+		return record
+	} catch (e) {
+		console.error(e)
+
+		throw e
+	}
 }
 
-const updateReminder = (id, content) => {
+const updateReminder = async (id, content) => {
+	try {
+		await dbo.collection("reminders").updateOne({id}, {$set: content})
+	} catch (e) {
+		console.error(e)
 
+		throw e
+	}
 }
 
-const deleteReminder = (id) => {
+const deleteReminder = async (id) => {
+	try {
+		await dbo.collection("reminders").deleteOne({id})
+	} catch (e) {
+		console.error(e)
 
+		throw e
+	}
 }
 
 module.exports = {
